f3cfecdf6f5e49da2a999636e83cab9577172227
Diego M. Rivera <mrdiego.dev@gmail.com>
2025-07-19 20:04:38 -0600

    start nuxt 4 migration

---
+++ app/components/Content/Experience/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import Section from '../Section.vue';
+  import JobPosition from './JobPosition.vue';
+  import { experience } from '@@/i18n/en/experience';
+
+  const { jobs } = experience;
+</script>
+
+<template>
+  <Section id="experience" title="Experience">
+    <ol class="relative border-s border-charcoal-gray">
+      <JobPosition v-for="(job, index) in jobs" :achievements="job.achievements" :stack="job.stack" :index="index" />
+    </ol>
+  </Section>
+</template>
